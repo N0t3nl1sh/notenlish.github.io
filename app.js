950a0f0b38e369d2a6bfb89dbfec53f47f933bc1
N0t3nl1sh <71970100+N0t3nl1sh@users.noreply.github.com>
2022-09-27 22:06:09 +0300

    .

---
+++ app.js
@@ -0,0 +1,15 @@
+
+/* 
+const Observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    })
+});
+
+
+const HiddenElements = document.querySelectorAll("hidden"); // .hidden de olabilir
+HiddenElements.forEach((el) => Observer.observe(el));
+*/
