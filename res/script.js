4fccf1ed657d7ad9eafea35c3be2324b4e0ef9b6
Notenlish <71970100+Notenlish@users.noreply.github.com>
2023-03-06 00:10:53 +0300

    new design
    I've done it properly this time

--- res/script.js
+++ res/script.js
@@ -1,91 +1,65 @@
+console.log("Hello World!");
+const prevBtn = document.getElementById('left-button');
+const nextBtn = document.getElementById('right-button');
+let sections = document.querySelectorAll("section");
+sections = document.querySelectorAll("section");
+let currentIndex = 0;
 
+nextBtn.addEventListener('click', () => {
+  if (currentIndex === sections.length - 1) {
+    return;
+  }
+  currentIndex++;
+  console.log(currentIndex);
+  updateSections();
+});
 
+prevBtn.addEventListener('click', () => {
+  if (currentIndex === 0) {
+    return;
+  }
+  currentIndex--;
+  console.log(currentIndex);
+  updateSections();
+});
 
-var ww = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-ww -= 100;
-var wh = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-wh -= 100;
+function updateSections() {
+  if (currentIndex === 0) {
+    prevBtn.style.opacity = "0.5";
+  } else if (currentIndex === sections.length - 1) {
+    nextBtn.style.opacity = "0.5";
+  } else {
+    prevBtn.style.opacity = "1";
+    nextBtn.style.opacity = "1";
+  }
 
-console.log("Start");
-
-const balls = document.getElementsByClassName("ball");
-
-for (var i=0; i<balls.length; i++) {
-    randompos(balls[i]);
-}
-
-function randompos(element){
-    var l = String((Math.random()*ww)+50 ) + "px";
-    var t = String((Math.random()*-wh) ) + "px";
-
-    element.style.top = t;
-    element.style.left = l;   
-}
-
-var dirx = Math.floor( Math.random() * 4 )
-var diry = Math.floor( Math.random() * 4 )
-
-dirx = Math.cos(dirx) // -1 1 arası
-diry = Math.cos(diry)
-
-var wrapper = document.getElementsByClassName("wrapper")[0];
-var wrapperrect = wrapper.getBoundingClientRect();
-
-console.log( wrapperrect.top, wrapperrect.left, wrapperrect.height, wrapperrect.width );
-
-function move(){
-    for (var i=0; i<balls.length; i++) {
-        var val = parseInt(balls[i].style.left, 10);
-        balls[i].style.left = String(val + (dirx * 15)) + "px";
-        var val = parseInt(balls[i].style.top, 10);
-        balls[i].style.top = String(val + (diry * 10)) + "px";
-        
-        var visible = isInViewport(balls[i]);
-        if (visible == false) {
-            /*
-            if part[0] < ( -20 ) or part[0] > ( obj[0].width +20 ):  
-                    if obj[1].x > 0:
-                        part[0] = -5
-                    elif obj[1].x < 0: #sola doğru gidiyor
-                        part[0] = obj[0].width + 5
-                if part[1] < (-20 ) or part[1] > ((obj[0].height +20 )):
-                    if obj[1].y > 0:
-                        part[1] = -5
-                    elif obj[1].y < 0:
-                        part[1] = obj[0].height + 5 
-            */
-            if (parseInt(balls[i].style.left,10) > window.innerWidth) {
-                balls[i].style.left = "0" + "px";
-                console.log("going right");
-            }
-            else if (parseInt(balls[i].style.left,10) < 0) {
-                balls[i].style.left = String(window.innerWidth) + "px";
-                console.log("going left");
-            }
-
-            if (parseInt(balls[i].style.top,10) > (wrapperrect.height+wrapperrect.top)/3  ) {
-                balls[i].style.top = String(-(wrapperrect.height)) + "px";
-                console.log("going down");
-            }
-            else if (parseInt(balls[i].style.top,10) < 0) {
-                balls[i].style.top = String( (wrapperrect.height-(wrapperrect.top*2))/3   ) + "px";
-                console.log("going up");
-            }
-
-            //console.log(balls[i].style.top,balls[i].style.left, "vw:", window.innerWidth,window.innerHeight)
-            //balls[i].style.left = String( ((dirx+1)/2)*(ww-100) ) + "px";
+  sections.forEach((section, index) => {
+    if (index === currentIndex) {
+        section.classList.add('active');
+        section.classList.remove('left');
+        section.classList.remove('right');
+        section.style.left = "0px";
+    } else { // not the current section
+        section.classList.remove('active');
+        if (index > currentIndex) { // section is to the left of the section we want to show
+            section.classList.add('right');
+            section.classList.remove('left');
+        } else { // section is to the right of the section we want to show
+            section.classList.remove('right');
+            section.classList.add('left');
         }
     }
+  });
 }
 
-var myVar = setInterval(move, 4);
-
-function isInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+function copyDiscordName(text){
+  console.log("copyDiscordName");
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      alert("Copied the discord name to your clipboard: " + text);
+    })
+    .catch((err) => {
+      console.error("Could not copy text: ", err);
+    });
 }
