19d3a740443317067d332c145de46330d0edbbc4
Notenlish <71970100+Notenlish@users.noreply.github.com>
2023-06-23 21:44:19 +0300

    fix discord copy button not working

--- res/script.js
+++ res/script.js
@@ -52,15 +52,15 @@ addEventListener("DOMContentLoaded", () => {
       }
     });
   }
-
-  function copyDiscordName(text) {
-    navigator.clipboard
-      .writeText(text)
-      .then(() => {
-        alert("Copied the discord name to your clipboard: " + text);
-      })
-      .catch((err) => {
-        console.error("Could not copy text: ", err);
-      });
-  }
 });
+
+function copyDiscordName(text) {
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      alert("Copied the discord name to your clipboard: " + text);
+    })
+    .catch((err) => {
+      console.error("Could not copy text: ", err);
+    });
+}
